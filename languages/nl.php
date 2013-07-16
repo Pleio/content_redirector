497f4763e26b4c29327bf970127d1447d2ca3e24
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-07-16 16:53:53 +0200

    updated dutch language

--- languages/nl.php
+++ languages/nl.php
@@ -1,26 +1,19 @@
-<?php 
-
-	$language = array(
-		'content_redirector' => "Content Redirector",
-		
-		'content_redirector:add:title' => "Nieuwe content toevoegen",
-		
-		'content_redirector:selector:type' => "Content type",
-		'content_redirector:selector:type:info' => "Wat wil je toevoegen?",
-		
-		'content_redirector:selector:container' => "Locatie",
-		'content_redirector:selector:container:info' => "Waar wil je het toevoegen?",
-		
-		'content_redirector:selector:container:personal' => "Persoonlijk",
-		'content_redirector:selector:container:group' => "In een groep",
-		
-	
-		'content_redirector:selector:group' => "Groep",
-		'content_redirector:selector:group:info' => "In welke groep wil je het toevoegen?",
-		'content_redirector:selector:group:none' => "Er is geen groep beschikbaar waar je het geselecteerde content type kunt plaatsen.",
-		
-		'content_redirector:selector:add' => "Toevoegen",
-		
-	);
-
-	add_translation("nl", $language);
+<?php
+$language = array (
+  'content_redirector:selector:type:title:info' => 'Selecteer het type content wat je wilt toevoegen',
+  'content_redirector:selector:container:title:info' => 'Kies tussen "Persoonlijk" of "In een groep" als doel locatie. Indien "In een groep" niet beschikbaar is, ben je waarschijnlijk niet lid van een groep, of is de groepsfunctionaliteit uitgeschakeld.',
+  'content_redirector:selector:group:title:info' => 'Selecteer de groep voor de nieuwe content. Sommige groepen kunnen niet beschikbaar zijn indien ze niet de geselecteerde content type ondersteunen.',
+  'content_redirector' => 'Content Redirector',
+  'content_redirector:add:title' => 'Nieuwe content toevoegen',
+  'content_redirector:selector:type' => 'Content type',
+  'content_redirector:selector:type:info' => 'Wat wil je toevoegen?',
+  'content_redirector:selector:container' => 'Locatie',
+  'content_redirector:selector:container:info' => 'Waar wil je het toevoegen?',
+  'content_redirector:selector:container:personal' => 'Persoonlijk',
+  'content_redirector:selector:container:group' => 'In een groep',
+  'content_redirector:selector:group' => 'Groep',
+  'content_redirector:selector:group:info' => 'In welke groep wil je het toevoegen?',
+  'content_redirector:selector:group:none' => 'Er is geen groep beschikbaar waar je het geselecteerde content type kunt plaatsen.',
+  'content_redirector:selector:add' => 'Toevoegen',
+);
+add_translation("nl", $language);
